6ce68f760a51ecc14d746346ded94143e94718d0
Vijay Ravichander <vijaypreetham1@gmail.com>
2025-02-17 19:01:09 -0800

    Fix Build

--- apps/web/config.ts
+++ apps/web/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3005"
+export const BACKEND_URL = "https://api.photoai.vijayravichander.com"
 export const CLOUDFLARE_URL = "https://pub-147823d083d74652ac0a76bbe364f95d.r2.dev"
